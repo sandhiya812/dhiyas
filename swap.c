534afa84a5b5d4f5e067fef66fe81502e79f89d0
sandhiya812 <43338242+sandhiya812@users.noreply.github.com>
2019-02-26 11:38:54 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+      double firstNumber, secondNumber, temporaryVariable;
+      printf("Enter first number: ");
+      scanf("%lf", &firstNumber);
+      printf("Enter second number: ");
+      scanf("%lf",&secondNumber);
+      temporaryVariable = firstNumber;
+      firstNumber = secondNumber;
+      secondNumber = temporaryVariable;
+      printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
+      printf("After swapping, secondNumber = %.2lf", secondNumber);
+      return 0;
+getch();
+}
